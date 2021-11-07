524430098401cc24b8adfdd761bd99c1beb0d26f
fuzhengwei <184172133@qq.com>
2021-11-07 14:42:29 +0800

    小傅哥,feat：index 更新推送，验证webhook

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ ini_set("error_reporting", "E_ALL & ~E_NOTICE");
 
 echo "Hi，Webhooks！By 小傅哥<br/>";
 
-echo '<br/>输出项目路径和用户目录：<br/>';
+echo '<br/>测试：输出项目路径和用户目录：<br/>';
 
 exec("cd ~ && cd - && cd -", $output);
 
