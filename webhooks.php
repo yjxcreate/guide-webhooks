651d2605bc373aea8b14c1332474a44d1ad49216
fuzhengwei <184172133@qq.com>
2021-11-07 20:58:46 +0800

    小傅哥,feat：更新推送，验证webhook

--- webhooks.php
+++ webhooks.php
@@ -24,7 +24,7 @@ $path = "/www/wwwroot/39.96.73.167/guide-webhooks/";
 // 判断需要下拉的分支上是否有提交，我们这里的分支名称为 main
 if ($content['ref'] == 'refs/heads/main') {
 
-    // 执行pull代码脚本
+    // 执行脚本 git pull，拉取分支最新代码
     $res = shell_exec("cd {$path} && git pull origin main 2>&1"); // 当前为www用户
 
     // 记录日志 ($content 返回的是一整个对象，可以按需获取里面的内容，写入日志)
